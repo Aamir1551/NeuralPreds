6ea8e60c000139fd9a8996e625053712212ed492
aamir soni <ll17a26s@leeds.ac.uk>
2019-03-12 18:33:02 +0000

    mini network impleented and testing successful

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        InputNeuron n1 = new InputNeuron(1);
-        Neuron n2 = new Neuron(2);
+        InputNeuron n0 = new InputNeuron(1);
+        Neuron n_out = new Neuron(2);
 
-        Neuron.connect(n1, n2, 3.0);
 
-        n1.start();
-        System.out.println(n2.getValue());
+        ArrayList<Neuron> neuronList = new ArrayList<>();
+        for(int i = 0;i<10;i++) {
+           neuronList.add(new Neuron(1));
+        }
+
+        for(Neuron n : neuronList) {
+            Neuron.connect(n0, n, 2.0);
+            Neuron.connect(n, n_out, 3.0);
+        }
+
+
+
+        n0.start();
+        System.out.println(n_out.getValue());
 
 
     }
